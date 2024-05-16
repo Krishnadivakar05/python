0bf00d8593dd80411959c1d4c6e47ffa5bc53b7f
ArthiH <arthihari398@gmail.com>
2024-05-16 11:46:50 +0530

    file uploaded

---
+++ day 1.py
@@ -0,0 +1,36 @@
+print('''*
+* *
+*  *
+*   *
+*    *
+* * * *''')
+print('*')
+print('* *')
+print('*  *')
+print('*   *')
+print('*    *')
+print('* * * *')
+
+
+print("From:")
+print("\tkrishna divakar,\n\tABC college,\n\tPuducherry.")
+print("To:")
+print("\tPrincipal,\n\tABCcollege,\n\tPuducherry.")
+print()
+print("Respected sir/mam,\n")
+print("\tIam Krishnadivakar studying in your college No:6385819406.")
+print("As I was suffering from fever I can't able to atten class on 22-03-2024\nSo,I kindly request you to allow me to the class.Its a humble request.")
+print("\t\t\t\tThanking you,\n\n")
+print("\t\t\t\t\t\t\tYours Faithfully,")
+print("\t\t\t\t\t\t\tKrishnadivakar")
+
+
+print("-"*65)
+print('|\tSNO \t | \t Name   \t | \t Course \t|')
+print('-'*65)
+print('|\t1 \t | \t Alan   \t | \t Python \t|')
+print('|\t2 \t |\tKrishna          |\t Python \t|')
+print('-'*65)
+
+
+
